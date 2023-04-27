793d37003eb49fd381bc76edbaa07d992e1eb397
EvelynGar <128769901+EvelynGar@users.noreply.github.com>
2023-04-26 21:45:20 -0600

    Actualizacion

--- Assets/Scrpts/GameManager.cs
+++ Assets/Scrpts/GameManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
 	
@@ -13,38 +13,77 @@ public class GameManager : MonoBehaviour
 	public Transform CreateEnemy;
 	private int vidas = 3;
     internal static object Instancie;
+    public GameObject menuPrincipal;
+    public GameObject menuGameOver;
+    public bool gameOver = false;
+    public bool start = false;
+    Enemy enemigo;
 
-
-	public GameManager() { }
+    public GameManager() { }
     void Start()
     {
-       //InvokeRepeating("CrearEnemigos",5f,3f);
+        enemigo = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Enemy>();
+        InvokeRepeating("CrearEnemigos",5f,3f);
 		
     }
 
 
     void Update()
     {
-		
-        inputHorizontal = Input.GetAxis("Horizontal");
-		if(inputHorizontal > 0)
-		{
-			fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(inputHorizontal,0) * Time.deltaTime;
-		}
-		
-		
+        if (start == true && gameOver == true)
+        {
+            menuGameOver.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            }
+        }
+        if (start == false)
+        {
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                start = true;
+
+            }
+        }
+
+        if (start == true && gameOver == false)
+        {
+            menuPrincipal.SetActive(false);
+            inputHorizontal = Input.GetAxis("Horizontal");
+            if (inputHorizontal > 0)
+            {
+                fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(inputHorizontal, 0) * Time.deltaTime;
+            }
+
+        }
+
     }
-	
-	
-	public void CrearEnemigos()
-	{
-		Instantiate(enemigos, CreateEnemy.position,CreateEnemy.rotation);
-	}
+
+
+    public void CrearEnemigos()
+    {
+        if (start == true && gameOver == false)
+        {
+            Instantiate(enemigos, CreateEnemy.position, CreateEnemy.rotation);
+        }
+    }
 
 
 	public void PerderVidas()
 	{
-		vidas = -1;
-		new Hud().DesactivaeVida(vidas);
+		vidas--;
+        if(vidas <= 0)
+        {
+            gameOver = true;
+        }
+        else
+        {
+            hud.DesactivaeVida(vidas);
+        }
+		
+         
 	}
+ 
 }
