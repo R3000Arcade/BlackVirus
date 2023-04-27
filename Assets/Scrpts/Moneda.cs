90b797b0afd4af971993742c224d7903ed20f3c0
EvelynGar <128769901+EvelynGar@users.noreply.github.com>
2023-04-26 21:47:39 -0600

    Actualizacion

--- Assets/Scrpts/Moneda.cs
+++ Assets/Scrpts/Moneda.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Moneda : MonoBehaviour
 {
+   
     [SerializeField] private float cantidadPuntos;
 
     [SerializeField] private Puntaje puntaje;
+    int puntos = 0;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
             {
             puntaje.SumarPuntos(cantidadPuntos);
             Destroy(gameObject);
+            
         }
     }
 }
